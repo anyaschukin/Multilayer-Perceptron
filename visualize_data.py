b1d488433a2b4b7bc899070779934acb2d8a5169
Anya Schukin <anya.schukin@gmail.com>
2020-04-17 19:21:12 +0200

    WIP/add pairplot radius

--- visualize_data.py
+++ visualize_data.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 # from sklearn import preprocessing
 import numpy as np
+import tools as tools
 
 # data = pd.read_csv('./data 2.csv')
 
@@ -13,7 +14,13 @@ import numpy as np
 
 # print(data.head())
 
-
+def pairplot(data):
+    radius = data[['radius_mean','radius_se','radius_worst','diagnosis']]
+    sns.pairplot(radius, hue='diagnosis',palette="husl", markers=["o", "s"],height=4)
+    plt.show()
+    # try:
+    # except Exception:
+        # tools.error_exit('Failed to visualize data. Is data valid?')
 
 def scale(data):
     for feature in data:
@@ -26,9 +33,9 @@ def scale(data):
                 # print(value)
                 std_value = (value - value_min) / (value_max - value_min)
                 data[feature].replace(to_replace=value, value=std_value, inplace=True)
-    print(data)
+    # print(data)
     
-
+# remove 1) unnecessary columns and 2) columns with NaN
 def preprocess(data):
     # see if there are any columns with missing/null data
     # print(data.isnull().sum())
@@ -37,15 +44,16 @@ def preprocess(data):
         data['diagnosis'] = data['diagnosis'].map({'M':1, 'B':0})
     except Exception:
         tools.error_exit('Failed to preprocess data. Is data valid?')
-    data = scale(data)
+    # data = scale(data)
     # normalize here?
     return data
 
 def main():
     data = pd.read_csv('./data/data_labeled.csv')
-    print(data)
+    # print(data)
     # print(data.describe())
     data = preprocess(data)
+    pairplot(data)
 
 if __name__ == '__main__':
     main()
