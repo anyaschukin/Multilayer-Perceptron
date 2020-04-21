cf7d4841ad5b2f5985c7c7f950d9c6b3de9b2a05
Anya Schukin <anya.schukin@gmail.com>
2020-04-21 15:49:58 +0200

    feature/pair_plot - heat_map and strip_plot work

--- pair_plot.py
+++ pair_plot.py
@@ -4,8 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tools as tools
 
-def pair_plot(data):
+def strip_plot(data):
+    data_strip = data.drop('diagnosis',axis=1)
+    data_strip = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean']]
+    for index,columns in enumerate(data_strip):
+        plt.figure(figsize=(15,15))
+        sns.stripplot(x='diagnosis', y= columns, data= data, jitter=True, palette = 'Set1')
+        plt.title('Diagnosis vs ' + str(columns))
+        plt.show()
 
+def heat_map(features):
+    #draw a heatmap between mean features and diagnosis
+    # mean = ['radius_mean','texture_mean','perimeter_mean','area_mean','smoothness_mean', 'compactness_mean', 'concavity_mean','concave points_mean', 'symmetry_mean', 'fractal_dimension_mean']
+    # mean = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean','diagnosis']]
+    plt.figure(figsize=(15,10))
+    heat = sns.heatmap(features.corr(), vmax=1, square=True, annot=True, cmap="YlGnBu")
+    plt.show()
+
+def pair_plot(feature):
+    try:
+        sns.pairplot(feature, hue='diagnosis',palette="husl", markers=["o", "s"],height=4)
+        plt.show()
+    except Exception:
+        tools.error_exit('Failed to visualize data. Is data valid?')
+
+def select_feature(data):
     # mean = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean','diagnosis']]
     # se = data[['texture_se','perimeter_se','area_se','smoothness_se','compactness_se','concavity_se','concave points_se','symmetry_se','fractal_dimension_se','diagnosis']]
     # worst = data[['texture_worst','perimeter_worst','area_worst','smoothness_worst','compactness_worst','concavity_worst','concave points_worst','symmetry_worst','fractal_dimension_worst','diagnosis']]
@@ -22,9 +45,4 @@ def pair_plot(data):
     # fractal_dimension = data[['fractal_dimension_mean','fractal_dimension_se','fractal_dimension_worst','diagnosis']]
 
     feature = concavity
-
-    try:
-        sns.pairplot(feature, hue='diagnosis',palette="husl", markers=["o", "s"],height=4)
-        plt.show()
-    except Exception:
-        tools.error_exit('Failed to visualize data. Is data valid?')
+    return feature
