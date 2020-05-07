3fda985b489ff0250c038a7923a12d15901491b6
Anya Schukin <anya.schukin@gmail.com>
2020-05-07 13:27:41 +0200

    fix/output layer now outputting [train_size, 1] dim matrix... good!

--- neural_network.py
+++ neural_network.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import tools as tools
 import matplotlib.pyplot as plt
+import preprocess as prep
 
 
 # def relu_prime(z):
@@ -82,36 +83,44 @@ def compute_loss(y_hat, y):
 class NeuralNetwork:
     def __init__(self, x, y):
         self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1], NUM_NEURONS) 
-        self.weights2   = np.random.rand(NUM_NEURONS,1)
-        self.weights3   = np.random.rand(NUM_NEURONS,1)
-        self.weights4   = np.random.rand(NUM_NEURONS,1)
+        self.weights1   = np.random.rand(self.input.shape[1], NUM_NEURONS) * 0.01 # self.input.shape[1] is num_features
+        self.weights2   = np.random.rand(NUM_NEURONS,1) * 0.01
+        self.weights3   = np.random.rand(NUM_NEURONS,1) * 0.01
+        self.weights4   = np.random.rand(NUM_NEURONS, 1) * 0.01
+        # self.weights4   = np.random.rand(NUM_NEURONS, y.shape[0])
         # self.bias1       = np.zeros((self.input.shape[0], NUM_NEURONS))
-        self.bias1       = np.zeros((NUM_NEURONS, 1))
-        self.bias2       = np.zeros((y.shape[0], 1))
-        self.bias3       = np.zeros((y.shape[0], 1))
-        self.bias4       = np.zeros((y.shape[0], 1))
-        self.y          = y
-        self.output     = np.zeros(self.y.shape)
+        self.bias1       = np.random.rand(self.input.shape[0], 1)
+        self.bias2       = np.random.rand(1, NUM_NEURONS)
+        self.bias3       = np.random.rand(1, NUM_NEURONS)
+        self.bias4       = np.random.rand(1, 1)
+        # self.y          = y
+        self.y          = np.zeros((y.shape[0], 1))
+        self.output     = np.zeros((self.y.shape[0], 1))
 
-    def feedforward(self, activation = softmax):
+    def feedforward(self, activation = softmax, activation_hidden = sigmoid):
         
-        self.layer1 = activation(np.dot(self.input, self.weights1) + self.bias1) 
-        self.layer2 = activation(np.dot(self.layer1.T, self.weights2) + self.bias2)
-        self.layer3 = activation(np.dot(self.layer2.T, self.weights3) + self.bias3)
-        self.output = activation(np.dot(self.layer3.T, self.weights4) + self.bias4) # layer = theta(weight_l * a_l-1 + b_l)
+        self.layer1 = activation_hidden(np.dot(self.input, self.weights1) + self.bias1) 
+        # print(self.layer1.shape, self.weights1.shape)
+        self.layer2 = activation_hidden(np.dot(self.layer1, self.weights2) + self.bias2)
+        # print(self.layer2.shape, self.weights2.shape)
+        self.layer3 = activation_hidden(np.dot(self.layer2, self.weights3) + self.bias3)
+        print(self.layer3.shape)
+        self.output = activation(np.dot(self.layer3, self.weights4) + self.bias4) # layer = theta(weight_l * a_l-1 + b_l)
+        # print(self.output.shape, self.weights4.shape)
 
     ## application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-    def backprop(self, activation_derivative = softmax_prime, learning_rate = 0.1):
+    def backprop(self, activation_derivative = sigmoid_prime, learning_rate = 0.1):
 
         # output layer
-        d_Z4 = self.output - y
+        d_Z4 = self.output - self.y
+        print(d_Z4.shape)
         d_weights4 = np.dot(self.layer3.T, d_Z4)
         d_bias4 = np.sum(d_Z4, axis = 1, keepdims=True)
 
         # hidden layers
-        d_Z3 = np.dot(self.weights3.T, d_Z4) * activation_derivative(self.layer3)
-        d_weights3 = np.dot(self.layer2.T, d_Z3) # (layer-1) * output error
+        print(self.weights3.shape, d_Z4.shape)
+        d_Z3 = np.dot(self.weights3, d_Z4.T) * activation_derivative(self.layer3)
+        d_weights3 = np.dot(self.layer2, d_Z3.T) # (layer-1) * output error
         d_bias3 = np.sum(d_Z3, axis = 1, keepdims=True)
         
         d_Z2 = np.dot(self.weights2.T, d_Z3) * activation_derivative(self.layer2)
@@ -134,35 +143,42 @@ class NeuralNetwork:
         self.bias4 -= learning_rate * d_bias4
 
 
-X = np.array([[0,0,1],
-              [0,1,1],
-              [1,0,1],
-              [1,1,1]])
-y = np.array([[0],[1],[1],[0]])
-nn = NeuralNetwork(X,y)
+def main():
+    # X = np.array([[0,0,1],
+    #               [0,1,1],
+    #               [1,0,1],
+    #               [1,1,1]])
+    # y = np.array([[0],[1],[1],[0]])
 
-loss_values = []
+    data = pd.read_csv('./data/data_labeled.csv')
+    data = prep.preprocess(data)
+    # visualize(data)
+    train_set, test_set = prep.split(data)
 
-for i in range(1500):
-    nn.feedforward()
-    nn.backprop()
-    loss = compute_loss(nn.output, y)
-    loss_values.append(loss)
+    X, y = train_set.iloc[:, 1:], train_set.iloc[:, 0]
 
-print(nn.output)
-print(f" final loss : {loss}")
+    # X = numpy_array[:, 1:26]
+    # y = numpy_array[:, 0]
+    # X_train, X_test = X[:index], X[index:]
+    # y_train, y_test = y[:index], y[index:]
 
-plt.plot(loss_values)
-plt.show()
+    nn = NeuralNetwork(X,y)
+    loss_values = []
+    for i in range(1500):
+        nn.feedforward()
+        nn.backprop()
+        loss = compute_loss(nn.output, y)
+        loss_values.append(loss)
 
+    print(nn.output)
+    print(f" final loss : {loss}")
 
+    plt.plot(loss_values)
+    plt.show()
 
+if __name__ == '__main__':
+    main()
 
 
 # # softmax activation layer : compute values for each sets of scores in x
 # # not sure this works
-
-
-# a_layer = theta(weight_l * a_l-1 + b_l)
-
-
