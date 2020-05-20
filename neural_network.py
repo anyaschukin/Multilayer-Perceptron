fbf99ac479d41c06b8ed61c5417114b9fc28fa30
Anya Schukin <anya.schukin@gmail.com>
2020-05-20 11:33:50 +0200

    fix/leaky_Relu

--- neural_network.py
+++ neural_network.py
@@ -16,12 +16,35 @@ import preprocess as prep
 # def cost_prime(yHat, y):
 #     return yHat - y
 
+# cross entropy
+# def compute_loss(yHat, y):
+#     m = yHat.shape[1]
+#     cost = -1 / m * (np.dot(y, np.log(yHat).T) + np.dot(1 - y, np.log(1 - yHat).T))
+#     return np.squeeze(cost)
 
+    # Avoid division by zero
+    # yHat = np.clip(yHat, 1e-15, 1 - 1e-15)
+    # return - y * np.log(yHat) - (1 - y) * np.log(1 - yHat)
 
-LAYER1_NEURONS = 15
-LAYER2_NEURONS = 15
-LAYER3_NEURONS = 15
-LAYER4_NEURONS = 15
+    # predictions = np.clip(yHat, 1e-15, 1 - 1e-15)
+    # n = yHat.shape[0]
+    # ce = -np.sum(y*np.log(yHat+1e-9))/n
+    # return ce
+
+    # return np.where(y == 1, -np.log(yHat), -np.log(1 - yHat))
+    # if y == 1:
+    #   return -log(yHat)
+    # else:
+    #   return -log(1 - yHat)
+
+# MSE
+# def compute_loss(y_hat, y):
+    # return ((y_hat - y)**2).sum()
+
+LAYER1_NEURONS = 5
+LAYER2_NEURONS = 5
+LAYER3_NEURONS = 5
+LAYER4_NEURONS = 5
 
 # The ReLufunction performs a threshold operation to each input element 
 # where values less than zero are set to zero.
@@ -34,12 +57,12 @@ def ReLU_prime(Z):
 
 # leaky_ReLU
 def leaky_ReLU(z, alpha = 0.01):
-	return np.where(z >= 0, 1, z * alpha)
+	return np.where(z >= 0, z, z * alpha)
     # return max(alpha * z, z)
 
 def leaky_ReLU_prime(z, alpha = 0.01):
     return np.where(z >= 0, 1, alpha)
-	# return 1 if z > 0 else alpha
+	# return 1 if x > 0 else alpha
 
 # sigmoid
 def sigmoid(z):
@@ -79,32 +102,6 @@ def compute_loss(yhat, y):
     loss = -1/nsample * (np.sum(np.dot(np.log(yhat).T, y) + np.dot((1 - y), np.log(1 - yhat).T)))
     return loss
 
-# cross entropy
-# def compute_loss(yHat, y):
-#     m = yHat.shape[1]
-#     cost = -1 / m * (np.dot(y, np.log(yHat).T) + np.dot(1 - y, np.log(1 - yHat).T))
-#     return np.squeeze(cost)
-
-    # Avoid division by zero
-    # yHat = np.clip(yHat, 1e-15, 1 - 1e-15)
-    # return - y * np.log(yHat) - (1 - y) * np.log(1 - yHat)
-
-    # predictions = np.clip(yHat, 1e-15, 1 - 1e-15)
-    # n = yHat.shape[0]
-    # ce = -np.sum(y*np.log(yHat+1e-9))/n
-    # return ce
-
-    # return np.where(y == 1, -np.log(yHat), -np.log(1 - yHat))
-    # if y == 1:
-    #   return -log(yHat)
-    # else:
-    #   return -log(1 - yHat)
-
-# MSE
-# def compute_loss(y_hat, y):
-    # return ((y_hat - y)**2).sum()
-
-
 class NeuralNetwork:
     def __init__(self, x, y):
         self.input      = x
@@ -119,23 +116,30 @@ class NeuralNetwork:
         self.bias4       = np.random.rand(1, 1) # (1, num_of_classes)... maybe last layer shouldn't have bias
        
         self.y           = y.to_numpy().reshape(y.shape[0], 1)
-        # self.output     = np.zeros((self.y.shape[0], 1))
         self.output     = np.zeros((self.y.shape[0], 1))
 
-    def feedforward(self, activation = softmax, activation_hidden = sigmoid):
+    def feedforward(self, activation = softmax, activation_hidden = leaky_ReLU):
         
         self.layer1 = activation_hidden(np.dot(self.input, self.weights1) + self.bias1) 
         self.layer2 = activation_hidden(np.dot(self.layer1, self.weights2) + self.bias2)
         self.layer3 = activation_hidden(np.dot(self.layer2, self.weights3) + self.bias3)
         self.output = activation(np.dot(self.layer3, self.weights4) + self.bias4) # layer = theta(weight_l * a_l-1 + b_l)
         
+        # print("activation layer 1{}".format(activation_hidden(np.dot(self.input, self.weights1) + self.bias1)))
+        # print("activation layer 2{}".format(activation_hidden(np.dot(self.layer1, self.weights2) + self.bias2)))
+# 
+        # print("layer1 {}".format(self.layer1))
+        # print("layer2 {}".format(self.layer2))
+        # print("layer3 {}".format(self.layer3))
+        # print("output {}".format(self.output))
+
         # print("layer1 {0}, input {1}, weights1 {2}, bias {3}".format(self.layer1.shape, self.input.shape, self.weights1.shape, self.bias1.shape))
         # print("layer2 {0}, layer1 {1}, weights2 {2}, bias {3}".format(self.layer2.shape, self.layer1.shape, self.weights2.shape, self.bias2.shape))
         # print("layer3 {0}, layer2 {1}, weights3 {2}, bias {3}".format(self.layer3.shape, self.layer2.shape, self.weights3.shape, self.bias3.shape))
         # print("output {0}, layer3 {1}, weights4 {2}, bias {3}".format( self.output.shape, self.layer3.shape, self.weights4.shape, self.bias4.shape))
 
     ## application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-    def backprop(self, d_activation = softmax_prime, d_activation_hidden = sigmoid_prime, learning_rate = 0.01):
+    def backprop(self, d_activation = softmax_prime, d_activation_hidden = leaky_ReLU_prime, learning_rate = 0.01):
         m = self.input.shape[0] # num examples
         # weights and d_weights should have the same dimensions
 
@@ -166,10 +170,22 @@ class NeuralNetwork:
         self.weights3 -= learning_rate * d_weights3
         self.weights4 -= learning_rate * d_weights4
 
+        # print
+        # print("weights1 {}".format(self.weights1))
+        # print("weights2 {}".format(self.weights2))
+        # print("weights3 {}".format(self.weights3))
+        # print("weights4{}".format(self.weights4))
+
         self.bias1 -= learning_rate * d_bias1
         self.bias2 -= learning_rate * d_bias2
         self.bias3 -= learning_rate * d_bias3
         self.bias4 -= learning_rate * d_bias4
+
+        # print
+        # print("bias1 {}".format(self.bias1))
+        # print("bias2 {}".format(self.bias2))
+        # print("bias3 {}".format(self.bias3))
+        # print("bias4 {}".format(self.bias4))
 
         # print("d_Z4 {0} - layer3 {1} - d_weights4 {2} - d_bias4 {3}".format(d_Z4.shape, self.layer3.shape, d_weights4.shape, d_bias4.shape))
         # print("weights3 {} - d_Z4 {} - d_layer3 {}".format(self.weights3.shape, d_Z4.T.shape, activation_derivative(self.layer3).shape))
@@ -203,22 +219,23 @@ def main():
     # X_train, X_test = X[:index], X[index:]
     # y_train, y_test = y[:index], y[index:]
 
-    nn = NeuralNetwork(X,y)
+    nn = NeuralNetwork(X, y)
     loss_values = []
     for i in range(1500):
         nn.feedforward()
         nn.backprop()
         loss = compute_loss(nn.output, nn.y)
+        print("loss = {}".format(loss))
         loss_values.append(loss)
 
     # print(nn.output.shape)
     # print(nn.y.shape)
 
-    print(nn.output)
+    # print(nn.output)
     print(f" final loss : {loss}")
 
-    plt.plot(loss_values)
-    plt.show()
+    # plt.plot(loss_values)
+    # plt.show()
 
 if __name__ == '__main__':
     main()
