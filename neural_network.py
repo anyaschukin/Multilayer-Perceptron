54109c616542ff910c09896d6fd460ec980e0d41
Anya Schukin <anya.schukin@gmail.com>
2020-06-05 14:55:11 +0200

    WIP/softmax values add up to - + 1!

--- neural_network.py
+++ neural_network.py
@@ -53,6 +53,34 @@ def get_accuracy(Y_hat, Y):
 # def compute_loss(y_hat, y):
     # return ((y_hat - y)**2).sum()
 
+# derivative for f is f(1-f) for respective cost functions
+# THIS IS CORRECT FOR sigmoid and softmax
+# def activation_derivative(activation):
+    # return activation * (1.0 - activation)
+
+# def activation_derivative(z, f):
+# 	if fn == SIGMOID:
+# 		f = sigmoid
+# 	elif fn == SOFTMAX:
+# 		f = softmax
+# 	return f(z)*(1-f(z))
+
+# def accuracy(Y_hat, Y):
+    # Y_hat_ = convert_prob_into_class(Y_hat)
+    # return (Y_hat_ == Y).all(axis=0).mean()
+
+# def softmax(x):
+#     z = np.exp(x) / np.exp(x).sum(axis=1) [:,None]
+#     return z
+   
+    # return np.exp(z) / np.sum(np.exp(z)) 
+
+	# e = np.exp(float(z))
+	# return (e/np.sum(e))
+
+# def softmax_prime(self,grad):
+    # return self.old_y * (grad -(grad * self.old_y).sum(axis=1)[:,None])
+
 LAYER1_NEURONS = 16
 LAYER2_NEURONS = 16
 LAYER3_NEURONS = 16
@@ -90,59 +118,30 @@ def sigmoid_prime(z):
   return sigmoid(z) * (1-sigmoid(z))
 
 
-# def softmax(x):
-#     z = np.exp(x) / np.exp(x).sum(axis=1) [:,None]
-#     return z
-
-# def softmax_prime(self,grad):
-    # return self.old_y * (grad -(grad * self.old_y).sum(axis=1)[:,None])
-
 # what I was using when the NN wasn't learning
 def softmax(z):
     # Numerically Stable: (z - np.max(z) shifts the values of z so that the highest number is 0... [1, 3, 5] -> [-4, -2, 0]
     e = np.exp(z - np.max(z))
-    return e / e.sum()
+    return e / e.sum(axis=1)
     # return z
-
-    # return np.exp(z) / np.sum(np.exp(z)) 
-
-	# e = np.exp(float(z))
-	# return (e/np.sum(e))
 
 def softmax_prime(z):
   return softmax(z) * (1-softmax(z))
 
-# derivative for f is f(1-f) for respective cost functions
-# THIS IS CORRECT FOR sigmoid and softmax
-# def activation_derivative(activation):
-    # return activation * (1.0 - activation)
-
-# def activation_derivative(z, f):
-# 	if fn == SIGMOID:
-# 		f = sigmoid
-# 	elif fn == SOFTMAX:
-# 		f = softmax
-# 	return f(z)*(1-f(z))
-
-# def accuracy(Y_hat, Y):
-    # Y_hat_ = convert_prob_into_class(Y_hat)
-    # return (Y_hat_ == Y).all(axis=0).mean()
-
-epsilon = 1e-5  
+epsilon = 1e-2  
 
 # binary cross-entropy loss
 def compute_loss(yhat, y):
-    # m = yhat.shape[1]
-    # print("yhat {}, y {}".format(yhat, y))
     m = len(y)
-    # return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
-    # loss = -1/m * (np.sum(np.nan_to_num((np.dot(np.log(yhat + epsilon).T, y) + np.dot((1 - y).T, np.log(1 - yhat + epsilon))))))
-    loss = -1/m * (np.sum(np.dot(np.log(yhat + epsilon).T, y) + np.dot((1 - y).T, np.log(1 - yhat + epsilon))))
+    # loss = -1/m * (np.sum(np.dot(np.log(yhat + epsilon).T, y) + np.dot((1 - y).T, np.log(1 - yhat + epsilon))))
+    loss = -1/m * (np.sum(np.dot(np.log(yhat).T, y) + np.dot((1 - y).T, np.log(1 - yhat))))
     return loss
 
+    # return np.sum(np.nan_to_num(-y*np.log(yhat)-(1-y)*np.log(1-yhat)))
+    # loss = -1/m * (np.sum(np.nan_to_num((np.dot(np.log(yhat + epsilon).T, y) + np.dot((1 - y).T, np.log(1 - yhat + epsilon))))))    
+
 def compute_loss_prime(yhat, y):
-    d_loss = - (np.divide(y, yhat) - np.divide(1 - y, 1 - yhat))
-    # print("d_loss = {}".format(d_loss))
+    d_loss = - (np.divide(y, yhat) - np.divide(1 - y, 1 - yhat)) ## not sure if this should be - or +, according to Kaggle example
     return d_loss
 
 class NeuralNetwork:
@@ -158,12 +157,9 @@ class NeuralNetwork:
         self.bias3       = np.zeros((1, LAYER3_NEURONS))
         self.bias4       = np.zeros((1, 2)) # (1, num_of_classes)... maybe last layer shouldn't have bias
        
-        self.y           = y.to_numpy().reshape(y.shape[0], 1)
+        # self.y           = y.to_numpy().reshape(y.shape[0], 1)
+        self.y           = y
         self.output     = np.zeros((self.y.shape[0], 2))
-
-        # print("self.y {}".format(self.y))
-        # print("output {}".format(self.output))
-        # print("weights 1 {}".format(self.weights1))
 
     def feedforward(self, activation = softmax, activation_hidden = sigmoid):
         
@@ -177,34 +173,17 @@ class NeuralNetwork:
         self.output = activation(self.Z4) # layer = theta(weight_l * a_l-1 + b_l)
         print("output =  \n{}\n target = \n{}".format(self.output, self.y))
 
-        
-        # self.layer1 = activation_hidden(np.dot(self.input, self.weights1) + self.bias1) 
-        # self.layer2 = activation_hidden(np.dot(self.layer1, self.weights2) + self.bias2)
-        # self.layer3 = activation_hidden(np.dot(self.layer2, self.weights3) + self.bias3)
-        # self.output = activation(np.dot(self.layer3, self.weights4) + self.bias4) # layer = theta(weight_l * a_l-1 + b_l)
-
-        # print("activation layer 1{}".format(activation_hidden(np.dot(self.input, self.weights1) + self.bias1)))
-        # print("activation layer 2{}".format(activation_hidden(np.dot(self.layer1, self.weights2) + self.bias2)))
-# 
-        # print("layer1 {}".format(self.layer1))
-        # print("layer2 {}".format(self.layer2))
-        # print("layer3 {}".format(self.layer3))
-        # print("output {}".format(self.output))
-
-        # print("layer1 {0}, input {1}, weights1 {2}, bias {3}".format(self.layer1.shape, self.input.shape, self.weights1.shape, self.bias1.shape))
-        # print("layer2 {0}, layer1 {1}, weights2 {2}, bias {3}".format(self.layer2.shape, self.layer1.shape, self.weights2.shape, self.bias2.shape))
-        # print("layer3 {0}, layer2 {1}, weights3 {2}, bias {3}".format(self.layer3.shape, self.layer2.shape, self.weights3.shape, self.bias3.shape))
-        # print("output {0}, layer3 {1}, weights4 {2}, bias {3}".format( self.output.shape, self.layer3.shape, self.weights4.shape, self.bias4.shape))
 
     ## application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
     def backprop(self, d_activation = softmax_prime, d_activation_hidden = sigmoid_prime, learning_rate = 0.1):
         m = self.input.shape[0] # num examples or batch size
         # weights and d_weights should have the same dimensions
 
-        d_A4 = compute_loss_prime(self.output, self.y)
-        
-        # d_A4 = -self.y/self.output + (1-self.y)/(1-self.output) # from Kaggle dataset
         # d_Z4 = d_A4 * d_activation(self.output) # not sure if we need activation derivative on output
+        # d_A4 = -self.y/self.output + (1-self.y)/(1-self.output) # from Kaggle dataset
+
+        # d_A4 = compute_loss_prime(self.output, self.y)
+        d_A4 = self.output - self.y
 
         # output layer
         d_Z4 = d_A4 * d_activation(self.Z4) # not sure if we need activation derivative on output
@@ -270,7 +249,7 @@ class NeuralNetwork:
         # d_weights1 = np.dot(self.input.T, d_Z1) # (layer-1) * output error
         # d_bias1 = np.sum(d_Z2, axis = 0, keepdims=True)
        
-        ## update the weights with the derivative (slope) of the loss function
+        ## update the weights with the derivative (slope) of the loss function (SGD)
         self.weights1 -= learning_rate * (d_weights1 / m) 
         self.weights2 -= learning_rate * (d_weights2 / m)
         self.weights3 -= learning_rate * (d_weights3 / m)
@@ -316,7 +295,20 @@ def main():
     train_set, test_set = prep.split(data)
 
     # X, y = train_set.iloc[:, 1:], train_set.iloc[:, 0]
-    X, y = train_set.iloc[:10, 1:], train_set.iloc[:10, 0]
+    X, y = train_set.iloc[:2, 1:], train_set.iloc[:2, 0]
+
+    target = np.zeros((y.shape[0], 2))
+    target[np.arange(y.size),y] = 1     # transform into one-hot encoding vector
+
+    y = target
+
+    # for row in y:
+        # if row == 1: #if malignant
+            # target[row] = [0,1]
+        # if row == 0: #if benign (can only be labeled 'B' or 'M')
+            # target[row] = [1,0]
+    
+    print("y \n{}\n target \n{}\n".format(y, target))
 
     # print("X {}".format(X))
     # print("y {}".format(y))
@@ -334,7 +326,7 @@ def main():
     nn = NeuralNetwork(X, y)
     loss_values = []
 
-    for i in range(20000):
+    for i in range(2000):
         nn.feedforward()
         # if i == 1000:
             # plt.scatter(nn.y, nn.output)
