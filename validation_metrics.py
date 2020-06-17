af3207fe51e919cacb4190abae890bd10d106812
Anya Schukin <anya.schukin@gmail.com>
2020-06-17 18:45:13 +0200

    fix/remove accuracy from get_validation_metrics

--- validation_metrics.py
+++ validation_metrics.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import numpy as np
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # an auxiliary function that converts probability into class
 def probability_to_class(yhat):
     probs = np.copy(yhat)
@@ -29,4 +39,6 @@ def get_validation_metrics(y_pred, y_true):
     specificity = tn / (tn + fp)
     F1_score = (2 * (precision * recall)) / (precision + recall)
 
-    return precision, recall, specificity, F1_score
+    print("precision = {}\nrecall = {}\nspecificity = {}\nF1_score = {}\n\n".format(precision, recall, specificity, F1_score))
+
+    # return precision, recall, specificity, F1_score
