fb98188f20f0f8471d6bedf33c6e9a6fd4991927
Anya Schukin <anya.schukin@gmail.com>
2020-06-19 14:54:26 +0200

    refacto/move parse_args into tools file

--- tools.py
+++ tools.py
@@ -1,31 +1,46 @@
 import os
 import sys
+import argparse
 
 def error_exit(err_msg):
-	print('Error: {}' .format(err_msg))
-	sys.exit()
+    print('Error: {}' .format(err_msg))
+    sys.exit()
 
 def is_file(path):
-	if not os.path.isfile(path):
-		error_exit('path specified ({}) does not exist' .format(path))
+    if not os.path.isfile(path):
+        error_exit('path specified ({}) does not exist' .format(path))
 
-def parse_args(usage):
-	my_parser = argparse.ArgumentParser(description=usage)
+def parse_args():
+	my_parser = argparse.ArgumentParser(description="Multilayer Perceptron predicts whether a cancer is malignant or benign.")
 	my_parser.add_argument('Dataset',
-		metavar='dataset',
-		type=str,
-		help='the path to dataset')
+					   metavar='dataset',
+					   type=str,
+					   help='provide a valid dataset.')
+	my_parser.add_argument('-d',
+						'--data_visualize',
+						action='store_true',
+						help='display data with graphs')
 	my_parser.add_argument('-t',
-		'--timer',
-		action='store_true',
-		help='Display time taken. Default false')
-	my_parser.add_argument('-c',
-		'--cost',
-		action='store_true',
-		help='Display cost graph, prediction error over training period. Default false')
+						'--train',
+						action='store_true',
+						help='use backpropagation and gradient descent to learn on the training dataset and save the model')
+	my_parser.add_argument('-p',
+						'--predict',
+						action='store_true',
+						help='perform a prediction on a given set')
+	my_parser.add_argument('-m',
+						'--mini_batch',
+						action='store_true',
+						help='train neural network on mini-batch, of size 32')
+	my_parser.add_argument('-e',
+						'--evaluation',
+						action='store_true',
+						help='display in-depth learning evaluation metrics')
 	args = my_parser.parse_args()
-	path = args.Dataset
-	data = tools.read_csv(path)
-	timer = args.timer
-	cost = args.cost
-	return data, timer, cost
+	dataset = args.Dataset
+	data_visualize = args.data_visualize
+	train = args.train
+	predict = args.predict
+	mini_batch = args.mini_batch
+	evaluation = args.evaluation
+	return dataset, data_visualize, train, predict, mini_batch, evaluation
