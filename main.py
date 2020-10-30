259087ff91dccad8cb5893597523ecaa85779680
Anya Schukin <anya.schukin@gmail.com>
2020-10-30 22:08:18 +0100

    refacto/messing around with try except protections

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from visualize import visualize
 from neural_network import train_model
 
 def main():
-	try:
-		dataset, data_visualize, train, predict, mini_batch, evaluation = parse_args()
+	# try:
+	dataset, data_visualize, train, predict, mini_batch, evaluation = parse_args()
 
-		data = pd.read_csv(dataset)
+	data = pd.read_csv(dataset)
+	data = preprocess(data)
+
+	if data_visualize:
+		visualize(data)
 		sys.exit(1) 
 
-		data = preprocess(data)
-		if data_visualize:
-			visualize(data)
-			sys.exit(1) 
+	train_model(data, train, predict, mini_batch, evaluation)
 
-		train_model(data, train, predict, mini_batch, evaluation)
-		
-	except:
-		print("\nError. You did something wrong.\n")
-		pass
+	# except:
+		# print("\nError. You did something wrong.\n")
+		# pass
 
 if __name__ == '__main__':
 	main()
