8a0fcb0ee11e3c2f6dbebdb4ea6bd99c4591e3eb
Anya Schukin <anya.schukin@gmail.com>
2020-10-30 21:45:56 +0100

    refacto/add try except

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from visualize import visualize
 from neural_network import train_model
 
 def main():
-	# try:
-	dataset, data_visualize, train, predict, mini_batch, evaluation = parse_args()
+	try:
+		dataset, data_visualize, train, predict, mini_batch, evaluation = parse_args()
 
-	data = pd.read_csv(dataset)
-	data = preprocess(data)
-	if data_visualize:
-		visualize(data)
+		data = pd.read_csv(dataset)
 		sys.exit(1) 
 
-	train_model(data, train, predict, mini_batch, evaluation)
-	# except:
-		# pass
+		data = preprocess(data)
+		if data_visualize:
+			visualize(data)
+			sys.exit(1) 
+
+		train_model(data, train, predict, mini_batch, evaluation)
+		
+	except:
+		print("\nError. You did something wrong.\n")
+		pass
 
 if __name__ == '__main__':
 	main()
