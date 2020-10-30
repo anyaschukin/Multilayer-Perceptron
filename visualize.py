085726f7404b134a38922d71b838ab96d995ef22
Anya Schukin <anya.schukin@gmail.com>
2020-10-30 15:19:38 +0100

    feature/can select from features for strip plot + heatmap + pair plot

--- visualize.py
+++ visualize.py
@@ -5,55 +5,75 @@ import time
 # import numpy as np
 import tools as tools
 
-def pair_plot(features):
+def pair_plot(feature):
 	try:
-		sns.pairplot(features, hue='diagnosis',palette="husl", markers=["o", "s"],height=4)
+		sns.pairplot(feature, hue='diagnosis',palette="husl", markers=["o", "s"],height=4)
 		plt.show()
 	except Exception:
 		tools.error_exit('Failed to visualize data. Is data valid?')
 
-def heat_map(data):
-	mean = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean','diagnosis']]
+def heat_map(feature):
+	# mean = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean','diagnosis']]
 	plt.figure(figsize=(15,10))
-	heat = sns.heatmap(mean.corr(), vmax=1, square=True, annot=True, cmap="YlGnBu")
+	heat = sns.heatmap(feature.corr(), vmax=1, square=True, annot=True, cmap="YlGnBu")
 	plt.show()
 
-def strip_plot(data):
-	data_strip = data.drop('diagnosis',axis=1)
-	data_strip = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean']]
+def strip_plot(feature):
+	data_strip = feature.drop('diagnosis',axis=1)
+	data_strip = feature[feature.columns.tolist()]
+	# data_strip = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean']]
 	for index,columns in enumerate(data_strip):
 		plt.figure(figsize=(15,10))
-		sns.stripplot(x='diagnosis', y= columns, data= data, jitter=True, palette = 'Set1')
+		sns.stripplot(x='diagnosis', y= columns, data= feature, jitter=True, palette = 'Set1')
 		plt.title('Diagnosis vs ' + str(columns))
 		plt.show()
 
 def select_feature(data):
+	options = ["mean", "se", "worst", "radius", "texture", "perimeter", "area", "smoothness","compactness", "concavity", "concave points", "symmetry", "fractal dimension"]
+
+	for i in range(len(options)):
+		print(str(i+1) + ":", options[i])
+
+	while True:
+		choice = int(input("Enter a number: "))
+		try:
+			choice = options[choice-1]
+			break
+		except:
+			print("Invalid input! Let's try that again.")
 
 	mean = data[['texture_mean','perimeter_mean','area_mean','smoothness_mean','compactness_mean','concavity_mean','concave points_mean','symmetry_mean','fractal_dimension_mean','diagnosis']]
-	# se = data[['texture_se','perimeter_se','area_se','smoothness_se','compactness_se','concavity_se','concave points_se','symmetry_se','fractal_dimension_se','diagnosis']]
-	# worst = data[['texture_worst','perimeter_worst','area_worst','smoothness_worst','compactness_worst','concavity_worst','concave points_worst','symmetry_worst','fractal_dimension_worst','diagnosis']]
-	##
-	# radius = data[['radius_mean','radius_se','radius_worst','diagnosis']]
-	# texture = data[['texture_mean','texture_se','texture_worst','diagnosis']]
-	# perimeter = data[['perimeter_mean','perimeter_se','perimeter_worst','diagnosis']]
-	# area = data[['area_mean','area_se','area_worst','diagnosis']]
-	# smoothness = data[['smoothness_mean','smoothness_se','smoothness_worst','diagnosis']]
-	# compactness = data[['compactness_mean','compactness_se','compactness_worst','diagnosis']]
-	# concavity = data[['concavity_mean','concavity_se','concavity_worst','diagnosis']]
-	# concave_points = data[['concave points_mean','concave points_se','concave points_worst','diagnosis']]
-	# symmetry = data[['symmetry_mean','symmetry_se','symmetry_worst','diagnosis']]
-	# fractal_dimension = data[['fractal_dimension_mean','fractal_dimension_se','fractal_dimension_worst','diagnosis']]
+	se = data[['texture_se','perimeter_se','area_se','smoothness_se','compactness_se','concavity_se','concave points_se','symmetry_se','fractal_dimension_se','diagnosis']]
+	worst = data[['texture_worst','perimeter_worst','area_worst','smoothness_worst','compactness_worst','concavity_worst','concave points_worst','symmetry_worst','fractal_dimension_worst','diagnosis']]
+	radius = data[['radius_mean','radius_se','radius_worst','diagnosis']]
+	texture = data[['texture_mean','texture_se','texture_worst','diagnosis']]
+	perimeter = data[['perimeter_mean','perimeter_se','perimeter_worst','diagnosis']]
+	area = data[['area_mean','area_se','area_worst','diagnosis']]
+	smoothness = data[['smoothness_mean','smoothness_se','smoothness_worst','diagnosis']]
+	compactness = data[['compactness_mean','compactness_se','compactness_worst','diagnosis']]
+	concavity = data[['concavity_mean','concavity_se','concavity_worst','diagnosis']]
+	concave_points = data[['concave points_mean','concave points_se','concave points_worst','diagnosis']]
+	symmetry = data[['symmetry_mean','symmetry_se','symmetry_worst','diagnosis']]
+	fractal_dimension = data[['fractal_dimension_mean','fractal_dimension_se','fractal_dimension_worst','diagnosis']]
 
-	features = mean
-	return features
+	features = {"mean": mean, "se": se, "worst": worst, "radius": radius, "texture": texture, "perimeter": perimeter, "area": area, "smoothness": smoothness,"compactness": compactness, "concavity": concavity, "concave points": concave_points, "symmetry": symmetry, "fractal dimension": fractal_dimension}
+	return features[choice]
 
 def visualize(data):
 	print(data)
-	time.sleep(3)
+	input("\n\nPress Enter to continue...\n\n")
 	print(data.describe())
-	time.sleep(3)
+	input("\n\nPress Enter to continue...\n\n")
 
-	features = select_feature(data)
-	pair_plot(features)	 # can select by feature for pairplot
-	heat_map(data)		  # only mean
-	strip_plot(data)		# only mean
+	feature = select_feature(data)
+	pair_plot(feature)
+	heat_map(feature)
+	strip_plot(feature)
+
+def main():
+	data = pd.read_csv(dataset)
+	data = preprocess(data)
+	visualize(data)
+
+if __name__ == '__main__':
+	main()
